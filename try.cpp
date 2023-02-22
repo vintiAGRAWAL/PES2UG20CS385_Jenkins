dc38d765cf046f3ee329f719bf4a6ade67dce2f4
vintiAGRAWAL <89653560+vintiAGRAWAL@users.noreply.github.com>
2023-02-22 21:55:23 +0530

    Create try.cpp

---
+++ try.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Hello , from Vinti\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}

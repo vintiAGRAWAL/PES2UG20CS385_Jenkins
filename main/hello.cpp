e03754e9bf7463c3cc4c68117a34b6d001e12309
vintiAGRAWAL <89653560+vintiAGRAWAL@users.noreply.github.com>
2023-02-22 21:32:40 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "Hello , from Vinti\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
